3068dfab46de616b4c48a2a49bd1bcbe2f708617
abinrajaju <abinaju10109090@gmail.com>
2026-01-29 17:40:02 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,65 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+   texts: [
+    "ente shuttu,",
+    "yes, you right there,",
+    "today is all about you,",
+    "so let the world wait a little,",
+    "because it’s your special day!",
+    "I’m lucky to know you,",
+    "lucky to laugh with you,",
+    "lucky to see you smile,",
+    "even on your grumpy days,",
+    "you’re still my favorite person,",
+    "Did you make a birthday wish yet?",
+    "Remember to eat something tasty today,",
+    "keep your heart light and happy,",
+    "and enjoy every tiny moment,",
+    "I’ll be cheering for you quietly,",
+    "Happy Birthday  🌷✨",
+],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+   imgs: {
+    "Just for you,": "./imgs/songxinxin.jpeg",
+    "my precious girl,": "./imgs/nothing.jpg",
+    "today is a very special day,": "./imgs/shengri.jpeg",
+    "because it marks": "./imgs/zaiyiqi.png",
+    "our third birthday together!": "./imgs/nothing.jpg",
+    "Over these years, I’ve seen you,": "./imgs/yangzi.jpeg",
+    "being silly,": "./imgs/nothing.jpg",
+    "being adorable,": "./imgs/nothing.jpg",
+    "even being a little grumpy,": "./imgs/nothing.jpg",
+    "and I loved every version of you,": "./imgs/nothing.jpg",
+    "By the way… did you open all your gifts?": "./imgs/chuo.gif",
+    "This year, promise me you’ll eat well,": "./imgs/chihaodian.jpeg",
+    "fill your tummy completely,": "./imgs/weibao.jpeg",
+    "so you’ll have lots of energy,": "./imgs/nothing.jpg",
+    "to chase your dreams and miss me a little!": "./imgs/nothing.jpg",
+    "Happy Birthday, my love~~ 💕": "./imgs/nothing.jpg"
+},
+    
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "ivide click cheyy",
+        play: "MUSIC vende",
+        bannar_coming: "DECORATION vende",
+        balloons_flying: "ithu pora",
+        cake_fadein: "CAKE ",
+        light_candle: "CANDLE kathikk",
+        wish_message: "HAPPY BIRTHDAY mikkummoooo",
+        story: "enikk oru korach parayan ind"
+    },
+
+    // 结束语
+    loveText: 'Happy birthday mikku'
+};
